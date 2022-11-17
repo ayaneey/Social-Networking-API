b0279c3df50697bdbce0e5a1985617c3fa1150e5
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2022-11-17 23:17:07 +0000

    set up thought-route

---
+++ routes/api/thought-route.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+
+const {
+	getAllThought,
+	getThoughtById,
+	createThought,
+	updateThought,
+	deleteThought,
+	addReaction,
+	deleteReaction,
+} = require("../../controllers/thought-controller");
+
+// retrieve all thoughts // GET and POST
+router.route("/").get(getAllThought).post(createThought);
+
+// retrieve thoughts by id // GET, PUT and DELETE
+router
+	.route("/:id")
+	.get(getThoughtById)
+	.put(updateThought)
+	.delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
