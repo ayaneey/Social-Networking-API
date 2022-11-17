8b3ea897e76d9fa7816998853ed3ef732d8198be
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2022-11-17 23:34:49 +0000

    fully set up user-route

---
+++ routes/api/user-route.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const {
+	getAllUsers,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser,
+	addFriend,
+	removeFriend,
+} = require("../../controllers/userController");
+
+// retrieve all users // GET and POST
+router.route("/").get(getAllUsers).post(createUser);
+
+// retrieve users by id // GET, PUT and DELETE
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+router
+	.route("/:userId/friends/:friendsId")
+	.post(addFriend)
+	.delete(removeFriend);
