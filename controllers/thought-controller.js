7e194e0da4b3d722cc09ac3b7ae665be90949267
Ayan Ahmed <ayanahmed@Ayans-MacBook-Pro.local>
2022-11-17 20:32:02 +0000

    assigning new thought to user

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -8,3 +8,35 @@ const thoughtController = {
 			.catch((err) => res.status(500).json(err));
 	},
 };
+
+// retrieving a thought by its id
+getThoughtById({ params }, res);
+{
+	Thought.findOne({ _id: params.thoughtId })
+		.then((dbThoughtData) => {
+			if (!dbThoughtData) {
+				res.status(404).json({ message: "No thought with this id exists!" });
+				return;
+			}
+			res.json(dbThoughtData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(400).json(err);
+		});
+}
+
+// assigning new thought to user
+createThought({ body }, res);
+{
+	Thought.create({ thoughtText: body.thoughtText, username: body.username })
+		.then(({ _id }) =>
+			User.findOneAndUpdate(
+				{ _id: body.userId },
+				{ $push: { thoughts: _id } },
+				{ new: true }
+			)
+		)
+		.then((dbThoughtData) => res.json(dbThoughtData))
+		.catch((err) => res.status(400).json(err));
+}
