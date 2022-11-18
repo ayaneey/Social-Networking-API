952d9cbba5a3aed0a2c11a51c5257ccf6f5aeadb
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2022-11-18 13:41:57 +0000

    initializing function to format timestamp

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -15,3 +15,28 @@ const addDateSuffix = (date) => {
 
 	return dateStr;
 };
+
+// function to format timestamp
+module.exports = (
+	timestamp,
+	{ monthLength = "short", dateSuffix = true } = {}
+) => {
+	let months;
+
+	if (monthLength === "short") {
+		months = {
+			0: "Jan",
+			1: "Feb",
+			2: "Mar",
+			3: "Apr",
+			4: "May",
+			5: "Jun",
+			6: "Jul",
+			7: "Aug",
+			8: "Sep",
+			9: "Oct",
+			10: "Nov",
+			11: "Dec",
+		};
+	}
+};
