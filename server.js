aec184078190bf0b75255a7ab57fad93d54a8b75
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2022-11-22 13:25:18 +0000

    updated port number

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const db = require("./config/connection");
 const routes = require("./routes");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
